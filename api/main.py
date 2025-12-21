12a0e5b8a0b0468dd931b7cfc69f3fb1a94a06ed
Hope90 <84917980+Hope90@users.noreply.github.com>
2025-12-21 15:08:26 +0100

    Update main.py

--- api/main.py
+++ api/main.py
@@ -13,13 +13,6 @@ import os
 # App initialization
 # --------------------------------------------------
 app = FastAPI(title="Credit Scoring API")
-# Mount the 'dashboard' folder as static file.
-app.mount("/dashboard", StaticFiles(directory="dashboard"), name="dashboard")
-
-@app.get("/")
-def read_index():
-    return FileResponse(os.path.join("dashboard", "index.html"))
-
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -81,5 +74,6 @@ def predict(request: ClientRequest):
         "threshold": best_threshold,
         "decision": decision
     }
+
 
 
