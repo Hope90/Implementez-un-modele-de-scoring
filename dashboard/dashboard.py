230fe6a92c33a30a3aca241f4c16e8cf907e9b0c
Hope90 <84917980+Hope90@users.noreply.github.com>
2025-12-30 23:54:25 +0100

    Update dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -43,7 +43,9 @@ for col in df_clients_fe.select_dtypes(include="bool").columns:
 # --------------------------
 # Select client
 # --------------------------
-client_id = st.selectbox("Select client ID", app_test["SK_ID_CURR"].tolist())
+#client_id = st.selectbox("Select client ID", app_test["SK_ID_CURR"].tolist())
+
+client_ids = app_test["SK_ID_CURR"].sort_values().tolist()
 
 client_id = st.selectbox(
     "Select a client ID",
@@ -52,7 +54,8 @@ client_id = st.selectbox(
 
 if client_id == "— Select a client —":
     st.stop()
-    
+
+
 # Get row indices
 client_row_raw = app_test[app_test["SK_ID_CURR"] == client_id].index[0]
 client_row_fe = df_clients_fe[df_clients_fe["SK_ID_CURR"] == client_id].index[0]
